5de5bddfadbeb2e4751748a67acef6b5092eb07c
Jose Andres Rendon Hoyos <joserendon67@hotmail.com>
2017-08-22 09:42:12 -0500

    RandomFaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FaceComponent } from './face/face.component';
+// Importamos el módulo http del paquete http de Angular
+import { HttpModule, JsonpModule } from '@angular/http';
 
+// Se lo pasamos a imports para que lo cargue en la aplicación
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,    
+    FaceComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
